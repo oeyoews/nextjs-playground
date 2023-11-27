21433d2bae570737a880acc7d1b849988bd0fab2
oeyoews <jyao4783@gmail.com>
2023-11-27 10:36:28 +0800

    add uuid

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import useUndoStore from './store';
+import { v4 as uuid } from 'uuid';
 
 function Counter() {
   const { count, increment, decrement, undo } = useUndoStore();
+  const [id, setId] = useState();
+
+  useEffect(() => {
+    // @ts-ignore
+    setId(uuid());
+  }, []);
 
   return (
     <div>
+      {id}
       <p>Count: {count}</p>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Increment</button>
